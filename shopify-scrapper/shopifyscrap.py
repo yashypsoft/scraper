2545bbf3c6962def022f941c9840556572027841
Yash Prajapati <yashypsoft@gmail.com>
2026-01-23 20:44:49 +0530

    Scraper data with python

--- shopify-scrapper/shopifyscrap.py
+++ shopify-scrapper/shopifyscrap.py
@@ -110,7 +110,7 @@ class CloudflareBypassClient:
                 'User-Agent': random.choice(self.user_agents),
                 'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
                 'Accept-Language': 'en-US,en;q=0.5',
-                'Accept-Encoding': 'gzip, deflate, br',
+                # 'Accept-Encoding': 'gzip, deflate, br',
                 'DNT': '1',
                 'Connection': 'keep-alive',
                 'Upgrade-Insecure-Requests': '1',
