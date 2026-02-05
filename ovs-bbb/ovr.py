fc0d9557e0347019c05a00b1718f79d64fbd3d3b
Deval <bhattdeval735@gmail.com>
2026-02-05 11:16:02 +0530

    overstock and bbb combined fetch

--- ovs-bbb/ovr.py
+++ ovs-bbb/ovr.py
@@ -512,7 +512,7 @@ def extract_overstock_data(product_data: dict, product_url: str) -> List[Dict]:
 
                     if variation_id:
                         product_url = f"{product_url}?option={variation_id}"
-                        try
+                        try:
                             bbb_api_url = f"{BBB_API_BASE_URL}/{variation_id}"
                             bbb_data = fetch_json_bbb(bbb_api_url)
                             variant_info = extract_bbb_data(bbb_data)
